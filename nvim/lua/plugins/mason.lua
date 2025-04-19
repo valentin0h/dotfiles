08df37a55454c92480e5c9f60442cc61a9256b94
Valentino Hudhra <v.hudhra@gmail.com>
2025-04-19 13:44:31 +0200

    add blink.cmp

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -11,7 +11,7 @@ return {
     dependencies = { "williamboman/mason-lspconfig.nvim" },
     config = function()
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+      -- capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
       -- local lspconfig = require("lspconfig")
 
       -- Ensure the servers above are installed
