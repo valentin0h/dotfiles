fb68382c140a8a2aae9582680b0fe0cd0369052e
Valentino Hudhra <v.hudhra@gmail.com>
2025-07-18 08:37:59 +0200

    update ts_ls

--- nvim/lsp/ts_ls.lua
+++ nvim/lsp/ts_ls.lua
@@ -4,4 +4,15 @@ return {
   -- Filetypes to automatically attach to.
   filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
   root_markers = { "package.json" },
+
+  -- On attach function to set up organize imports keymap
+  on_attach = function(client, bufnr)
+    -- Organize imports keymap
+    vim.keymap.set("n", "<leader>co", function()
+      vim.lsp.buf.code_action({
+        context = { only = { "source.organizeImports" } },
+        apply = true,
+      })
+    end, { buffer = bufnr, desc = "Organize imports" })
+  end,
 }
