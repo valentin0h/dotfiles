3c8d50bb9fec6881d1040ebe9bd78012f1a0280d
Valentino Hudhra <v.hudhra@gmail.com>
2025-08-01 09:42:29 +0200

    fold column

--- nvim/lua/plugins/ufo.lua
+++ nvim/lua/plugins/ufo.lua
@@ -5,7 +5,7 @@ return {
       -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
       local ufo = require("ufo")
 
-      vim.o.foldcolumn = "1" -- '0' is not bad
+      vim.o.foldcolumn = "0" -- '0' is not bad
       vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
       vim.o.foldlevelstart = 99
       vim.o.foldenable = true
